db42d5086bce16981db810313019e3b0355e110f
Joserra <Joserra_013@hotmail.com>
2021-11-10 14:13:28 +0100

    Joserra version 5.0

--- app/src/main/java/com/iot/funwithmuseums/MainActivity.java
+++ app/src/main/java/com/iot/funwithmuseums/MainActivity.java
@@ -150,6 +150,7 @@ public class MainActivity extends AppCompatActivity implements ItemViewHolder.It
                 startActivity(chartgo);
             }
         });
+
         getCurrentLocation();
     }
 
